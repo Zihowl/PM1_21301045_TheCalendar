5e9a706a9f7fd3cfb7ae8cc06386312999d33c95
Zihowl <fernimacias777@gmail.com>
2025-07-25 03:20:14 -0600

    Add profile image support

--- app/src/main/java/com/zihowl/thecalendar/data/model/auth/AuthToken.java
+++ app/src/main/java/com/zihowl/thecalendar/data/model/auth/AuthToken.java
@@ -1,9 +1,17 @@
 package com.zihowl.thecalendar.data.model.auth;
 
+import com.google.gson.annotations.SerializedName;
+
 public class AuthToken {
     private String token;
+    @SerializedName("foto_perfil")
+    private String fotoPerfil;
 
     public String getToken() {
         return token;
+    }
+
+    public String getFotoPerfil() {
+        return fotoPerfil;
     }
 }
